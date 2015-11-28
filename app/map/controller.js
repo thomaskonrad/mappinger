5a043edffcaefa6c1a999ff174629621fc1dd08b
Thomas Konrad <mail@thomaskonrad.at>
2015-11-28 17:20:30 +0100

    Show preset suggestions correctly.

--- app/map/controller.js
+++ app/map/controller.js
@@ -49,7 +49,19 @@ mapControllers.controller('MapCtrl',  ['$scope', '$http', 'mapService',
                         return "point";
                     };
 
-                    data.type = presetLocales.en[idPresets.match(data).id].name;
+                    var preset = idPresets.match(data);
+
+                    if (preset !== undefined) {
+                        var id = preset.id;
+
+                        if (preset.suggestion) {
+                            id = id.split('/');
+                            id = id[0] + '/' + id[1];
+                        }
+
+                        data.type = presetLocales.en[id].name;
+                    }
+
                     $scope.selectedFeature = data;
                 });
 
