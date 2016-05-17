4f6364cc9b22571d38bcc5fffa88e3d51b28297a
Martin Grubinger <martin.grubinger@gmail.com>
2016-05-17 20:18:34 +0200

    fix missing class Coordinates

--- src/components/commons.ts
+++ src/components/commons.ts
@@ -1,5 +1,12 @@
-import {Coordinates} from './commons';
+export class Coordinates {
+    lat: number;
+    lon: number;
 
+    constructor(lat:number, lon:number) {
+        this.lat = lat;
+        this.lon = lon;
+    }
+}
 export enum FeatureType
 {
     Node = "node",
