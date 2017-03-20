05c882dbf891d9707f99f3643d34e98205dd14cd
Martin Grubinger <martin.grubinger@gmail.com>
2017-03-20 21:29:31 +0100

    adds commented enableProdMode for future reference

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,7 @@ import 'zone.js';
 import 'reflect-metadata';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule }              from './app.module';
+import { enableProdMode } from '@angular/core';
 
+// enableProdMode();
 platformBrowserDynamic().bootstrapModule(AppModule);
