7b5e59b6258692635124846c99a2c01c46e33f95
Thomas Konrad <mail@thomaskonrad.at>
2017-07-15 16:29:00 +0200

    Updated Webpack production config (debug deprecated, OccurenceOrderPlugin renamed).

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,13 +2,12 @@ var webpack = require("webpack"),
     webpackConfig = require('./webpack.config.js');
 
 webpackConfig.plugins.concat([
-    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin()
 ]);
 
 // remove source-maps
-webpackConfig.debug = false;
 webpackConfig.devtool = 'none';
 
 module.exports = webpackConfig;
